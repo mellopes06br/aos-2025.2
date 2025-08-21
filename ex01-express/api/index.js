66f6f0f96fc0a721c2c477d9351fa6ad26ed4488
mellopes06br <163359225+mellopes06br@users.noreply.github.com>
2025-08-21 13:42:18 +0000

    adicionando express

---
+++ ex01-express/api/index.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import cors from 'cors';
+import express from "express";
+
+
+console.log("MY_SECRET", process.env.MY_SECRET);
+console.log("PYTHON_ROOT", process.env.PYTHON_ROOT);
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+  res.send("Bem-vindo ao Express de Mel");
+});
+
+app.listen(3000, () => {
+  console.log("Example app listening on port 3000!");
+});
+
+ 
--- ex01-express/src/index.js
+++
@@ -1,5 +0,0 @@
-import 'dotenv/config';
-
-console.log("Olá! Estudar né pae");
-
-console.log(process.env.MY_SECRET);
